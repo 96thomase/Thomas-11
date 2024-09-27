9692c673d311fd22ffc26f68ef366fce6f326e34
96thomas <thomasenginring59@gmail.com>
2024-09-27 09:24:24 +0530

    index1-update commit index1-flie update

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.cros').computedStyleMap.display * 'none';
+document.querySelector('hamburger').addEventListener("click,()=>"){
+    document.querySelector('.sidebar').classList.toggle('siderbar');
+    
+}
