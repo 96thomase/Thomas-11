7bb1046c2fd329014a27e3d23d996fa03eee93f1
96thomas <thomasenginring59@gmail.com>
2024-12-03 20:14:55 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("script runing...");
-
-document.querySelector('.cross').style.display = "none";
+document.querySelector('.cross').style.display = 'none';
 document.querySelector('.hamburger').addEventListener("click", ()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebarGo');
-});
+    document.querySelector('.sidebar').classList.toggle('SidebarGo');
+})
+
+
